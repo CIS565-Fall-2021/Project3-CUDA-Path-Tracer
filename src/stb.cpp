75ecc16db4154566505bc81dd9d8ac88d658723b
LaurelinTheGold <richard.rl.chen@gmail.com>
2021-10-04 14:25:09 -0400

    added obj loader to stb

--- src/stb.cpp
+++ src/stb.cpp
@@ -1,6 +1,11 @@
 #define TINYGLTF_IMPLEMENTATION
 #include "tiny_gltf.h"
 
+#define TINYOBJLOADER_IMPLEMENTATION // define this in only *one* .cc
+// Optional. define TINYOBJLOADER_USE_MAPBOX_EARCUT gives robust trinagulation. Requires C++11
+// #define TINYOBJLOADER_USE_MAPBOX_EARCUT
+#include "tiny_obj_loader.h"
+
 #define STB_IMAGE_IMPLEMENTATION
 #include <stb_image.h>
 
