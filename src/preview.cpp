e5bb9cc43215edc800e476dc708b4c51aaec4dbe
Zhihao Ruan <ruanzh@seas.upenn.edu>
2021-10-21 18:07:48 -0400

    increase max color weight

--- src/preview.cpp
+++ src/preview.cpp
@@ -209,7 +209,7 @@ void drawGui(int windowWidth, int windowHeight) {
   ImGui::Checkbox("Denoise", &ui_denoise);
 
   ImGui::SliderInt("Filter Size", &ui_filterSize, 0, 100);
-  ImGui::SliderFloat("Color Weight", &ui_colorWeight, 0.0f, 10.0f);
+  ImGui::SliderFloat("Color Weight", &ui_colorWeight, 0.0f, 500.0f);
   ImGui::SliderFloat("Normal Weight", &ui_normalWeight, 0.0f, 10.0f);
   ImGui::SliderFloat("Position Weight", &ui_positionWeight, 0.0f, 10.0f);
 
