768bef4582039a3a870b4f594cdd471b0f6fa114
NuofanXu <nuofan.xu@mail.utoronto.ca>
2021-10-24 11:36:44 -0400

    changing year

--- src/sceneStructs.h
+++ src/sceneStructs.h
@@ -10,11 +10,24 @@
 enum GeomType {
     SPHERE,
     CUBE,
+    TRIANGLE,
 };
 
 struct Ray {
     glm::vec3 origin;
     glm::vec3 direction;
+};
+
+struct Point {
+    glm::vec3 pos;
+    glm::vec3 nor;
+    glm::vec2 uv;
+};
+
+struct Triangle {
+    Point pt1;
+    Point pt2;
+    Point pt3;
 };
 
 struct Geom {
@@ -26,6 +39,7 @@ struct Geom {
     glm::mat4 transform;
     glm::mat4 inverseTransform;
     glm::mat4 invTranspose;
+    Triangle triangle;
 };
 
 struct Material {
@@ -60,10 +74,10 @@ struct RenderState {
 };
 
 struct PathSegment {
-    Ray ray;
-    glm::vec3 color;
-    int pixelIndex;
-    int remainingBounces;
+	Ray ray;
+	glm::vec3 color;
+	int pixelIndex;
+	int remainingBounces;
 };
 
 // Use with a corresponding PathSegment to do:
