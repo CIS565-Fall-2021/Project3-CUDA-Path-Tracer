c413df66ffd6a2fb9309b2c77742af469f42f3fa
kaan <39895510+kaan9@users.noreply.github.com>
2021-12-28 01:40:41 -0500

    clean up utilities, remove unnecessary funcs.

--- src/utilities.cpp
+++ src/utilities.cpp
@@ -5,108 +5,93 @@
 //  A collection/kitchen sink of generally useful functions
 
 #include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/matrix_inverse.hpp>
 #include <iostream>
-#include <cstdio>
+#include <string>
+#include <sstream>
+#include <vector>
 
 #include "utilities.h"
 
-float utilityCore::clamp(float f, float min, float max) {
-    if (f < min) {
-        return min;
-    } else if (f > max) {
-        return max;
-    } else {
-        return f;
-    }
+float utilityCore::clamp(float f, float min, float max)
+{
+	return  f < min ? min :
+		f > max ? max :
+		f;
 }
 
-bool utilityCore::replaceString(std::string& str, const std::string& from, const std::string& to) {
-    size_t start_pos = str.find(from);
-    if (start_pos == std::string::npos)
-        return false;
-    str.replace(start_pos, from.length(), to);
-    return true;
+glm::vec3 utilityCore::clampRGB(glm::vec3 color)
+{
+	if (color[0] < 0)
+		color[0] = 0;
+	else if (color[0] > 255)
+		color[0] = 255;
+
+	if (color[1] < 0)
+		color[1] = 0;
+	else if (color[1] > 255)
+		color[1] = 255;
+
+	if (color[2] < 0)
+		color[2] = 0;
+	else if (color[2] > 255)
+		color[2] = 255;
+
+	return color;
 }
 
-std::string utilityCore::convertIntToString(int number) {
-    std::stringstream ss;
-    ss << number;
-    return ss.str();
+bool utilityCore::epsilon_eq(float a, float b)
+{
+	return fabs(fabs(a) - fabs(b)) < EPSILON;
 }
 
-glm::vec3 utilityCore::clampRGB(glm::vec3 color) {
-    if (color[0] < 0) {
-        color[0] = 0;
-    } else if (color[0] > 255) {
-        color[0] = 255;
-    }
-    if (color[1] < 0) {
-        color[1] = 0;
-    } else if (color[1] > 255) {
-        color[1] = 255;
-    }
-    if (color[2] < 0) {
-        color[2] = 0;
-    } else if (color[2] > 255) {
-        color[2] = 255;
-    }
-    return color;
+glm::mat4 utilityCore::make_transform_matrix(glm::vec3 translation, glm::vec3 rotation, glm::vec3 scale)
+{
+	return glm::translate(glm::mat4(), translation) *
+		glm::rotate(glm::mat4(), rotation.x * (float) PI / 180, glm::vec3(1, 0, 0)) *
+		glm::rotate(glm::mat4(), rotation.y * (float) PI / 180, glm::vec3(0, 1, 0)) *
+		glm::rotate(glm::mat4(), rotation.z * (float) PI / 180, glm::vec3(0, 0, 1)) *
+		glm::scale(glm::mat4(), scale);
 }
 
-bool utilityCore::epsilonCheck(float a, float b) {
-    if (fabs(fabs(a) - fabs(b)) < EPSILON) {
-        return true;
-    } else {
-        return false;
-    }
+std::vector<std::string> utilityCore::tokenize_string(std::string str)
+{
+	std::stringstream strstr(str);
+	std::istream_iterator<std::string> it(strstr);
+	std::istream_iterator<std::string> end;
+	std::vector<std::string> results(it, end);
+	return results;
 }
 
-glm::mat4 utilityCore::buildTransformationMatrix(glm::vec3 translation, glm::vec3 rotation, glm::vec3 scale) {
-    glm::mat4 translationMat = glm::translate(glm::mat4(), translation);
-    glm::mat4 rotationMat =   glm::rotate(glm::mat4(), rotation.x * (float) PI / 180, glm::vec3(1, 0, 0));
-    rotationMat = rotationMat * glm::rotate(glm::mat4(), rotation.y * (float) PI / 180, glm::vec3(0, 1, 0));
-    rotationMat = rotationMat * glm::rotate(glm::mat4(), rotation.z * (float) PI / 180, glm::vec3(0, 0, 1));
-    glm::mat4 scaleMat = glm::scale(glm::mat4(), scale);
-    return translationMat * rotationMat * scaleMat;
-}
+std::istream &utilityCore::safe_getline(std::istream &is, std::string &t)
+{
+	//Thanks to http://stackoverflow.com/a/6089413
+	t.clear();
 
-std::vector<std::string> utilityCore::tokenizeString(std::string str) {
-    std::stringstream strstr(str);
-    std::istream_iterator<std::string> it(strstr);
-    std::istream_iterator<std::string> end;
-    std::vector<std::string> results(it, end);
-    return results;
-}
+	// The characters in the stream are read one-by-one using a std::streambuf.
+	// That is faster than reading them one-by-one using the std::istream.
+	// Code that uses streambuf this way must be guarded by a sentry object.
+	// The sentry object performs various tasks,
+	// such as thread synchronization and updating the stream state.
 
-std::istream& utilityCore::safeGetline(std::istream& is, std::string& t) {
-    t.clear();
+	std::istream::sentry se(is, true);
+	std::streambuf *sb = is.rdbuf();
 
-    // The characters in the stream are read one-by-one using a std::streambuf.
-    // That is faster than reading them one-by-one using the std::istream.
-    // Code that uses streambuf this way must be guarded by a sentry object.
-    // The sentry object performs various tasks,
-    // such as thread synchronization and updating the stream state.
-
-    std::istream::sentry se(is, true);
-    std::streambuf* sb = is.rdbuf();
-
-    for (;;) {
-        int c = sb->sbumpc();
-        switch (c) {
-        case '\n':
-            return is;
-        case '\r':
-            if (sb->sgetc() == '\n')
-                sb->sbumpc();
-            return is;
-        case EOF:
-            // Also handle the case when the last line has no line ending
-            if (t.empty())
-                is.setstate(std::ios::eofbit);
-            return is;
-        default:
-            t += (char)c;
-        }
-    }
+	for (;;) {
+		int c = sb->sbumpc();
+		switch (c) {
+		case '\n':
+			return is;
+		case '\r':
+			if (sb->sgetc() == '\n')
+				sb->sbumpc();
+			return is;
+		case EOF:
+			// Also handle the case when the last line has no line ending
+			if (t.empty())
+				is.setstate(std::ios::eofbit);
+			return is;
+		default:
+			t += (char) c;
+		}
+	}
 }
--- src/utilities.h
+++ src/utilities.h
@@ -1,11 +1,7 @@
 #pragma once
 
 #include "glm/glm.hpp"
-#include <algorithm>
 #include <istream>
-#include <ostream>
-#include <iterator>
-#include <sstream>
 #include <string>
 #include <vector>
 
@@ -15,12 +11,12 @@
 #define EPSILON           0.00001f
 
 namespace utilityCore {
-    extern float clamp(float f, float min, float max);
-    extern bool replaceString(std::string& str, const std::string& from, const std::string& to);
-    extern glm::vec3 clampRGB(glm::vec3 color);
-    extern bool epsilonCheck(float a, float b);
-    extern std::vector<std::string> tokenizeString(std::string str);
-    extern glm::mat4 buildTransformationMatrix(glm::vec3 translation, glm::vec3 rotation, glm::vec3 scale);
-    extern std::string convertIntToString(int number);
-    extern std::istream& safeGetline(std::istream& is, std::string& t); //Thanks to http://stackoverflow.com/a/6089413
+
+float clamp(float f, float min, float max);
+glm::vec3 clampRGB(glm::vec3 color);
+bool epsilon_eq(float a, float b);
+std::vector<std::string> tokenize_string(std::string str);
+glm::mat4 make_transform_matrix(glm::vec3 translation, glm::vec3 rotation, glm::vec3 scale);
+std::istream &safe_getline(std::istream &is, std::string &t);
+
 }
